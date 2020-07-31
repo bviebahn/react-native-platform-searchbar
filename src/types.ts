9d8bb0dd25b9f79fdc00577f28c31458509e0ccc
Benedikt <bviebahn1993@gmail.com>
2020-07-31 12:54:47 +0200

    fix: typescript

--- src/types.ts
+++ src/types.ts
@@ -3,10 +3,27 @@ import type { TextInputProps, StyleProp, ViewStyle } from 'react-native';
 export type SearchBarProps = TextInputProps & {
     value: string;
     onChangeText(text: string): void;
+    /**
+     * SearchBar theme.
+     * Defaults to "light"
+     */
     theme?: 'light' | 'dark';
+    /**
+     * Text of the cancel button.
+     * Defaults to "Cancel"
+     * @platform ios
+     */
     cancelText?: string;
+    /**
+     * Color of icons (search, clear...).
+     * Inherits from placeholderTextColor
+     */
     iconColor?: string;
     placeholderTextColor?: string;
+    /**
+     * Custom icon to show on the left.
+     * Defaults to search icon
+     */
     leftIcon?: React.ReactElement;
     style?: StyleProp<ViewStyle>;
 };
